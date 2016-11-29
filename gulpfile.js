66f4d89cd130f0d908ff0c2b3bfa2d1a2724dbc3
Thijs Meeuwisse <thijs@twebber.nl>
2016-11-29 14:41:52 +0100

    Added a dummy index.html plus sublime setup

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
+var livereload = require('gulp-livereload')
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+
+gulp.task('default', ['sass', 'watch']);
+
+gulp.task('sass', function () {
+    return gulp.src('scss/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(autoprefixer())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('root/wp-content/themes/hypermedia'))
+    .pipe(livereload());
+});
+
+gulp.task('watch', function() {
+    livereload.listen();
+    gulp.watch('scss/**/*.scss', ['sass']);
+    gulp.watch('root/wp-content/themes/hypermedia/**/*.{php,js,amf,umf}', function() {
+        return gulp.src('root/wp-content/themes/hypermedia/**/*.php').pipe(livereload());
+    });
+});
