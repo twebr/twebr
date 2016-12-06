551badc64da17ab38e5fa77b62ad090cb4c955a4
Thijs Meeuwisse <thijs@twebber.nl>
2016-12-06 15:21:31 +0100

    Added portfolio items

--- js/form.js
+++ js/form.js
@@ -41,3 +41,42 @@
         });
     }(jQuery));
 }.call(this));
+
+// Check if form has been submitted
+var params = parseURLParams(window.location.search);
+
+// console.log(params);
+
+if(params !== undefined && params.name !== undefined) {
+    $(".js-form-confirmation").text("Thanks " + capitalizeFirstLetter(params.name[0]) + "! I received your message.");
+    console.log("Form filled out by " + params.name[0]);
+}
+
+function parseURLParams(url) {
+    var queryStart = url.indexOf("?") + 1,
+        queryEnd   = url.indexOf("#") + 1 || url.length + 1,
+        query = url.slice(queryStart, queryEnd - 1),
+        pairs = query.replace(/\+/g, " ").split("&"),
+        parms = {}, i, n, v, nv;
+
+    if (query === url || query === "") {
+        return;
+    }
+
+    for (i = 0; i < pairs.length; i++) {
+        nv = pairs[i].split("=");
+        n = decodeURIComponent(nv[0]);
+        v = decodeURIComponent(nv[1]);
+
+        if (!parms.hasOwnProperty(n)) {
+            parms[n] = [];
+        }
+
+        parms[n].push(nv.length === 2 ? v : null);
+    }
+    return parms;
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
